7249f7a8001c30a5f952e579ded38abe55346267
D-shelton <djstheii@gmail.com>
2024-04-07 10:10:53 -0500

    clean up function list, fix organize_letters, add alpha sorting, add report output

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ def main():
     word_count = count_the_words(text)
     raw_letters = count_the_letters(text)
     letter_count = organize_letters(raw_letters)
-    print(f"{word_count} words in this text, and the letters used break down as: {letter_count} ")
+
+    print(f"There are {word_count} words in this document")
+    for char_dict in letter_count:
+        print(f"The '{char_dict['char']}' character was found {char_dict['num']} times")
+        
 
 
 #### sub functions below in call order####
@@ -22,28 +26,17 @@ def count_the_letters(text):
     char_totals = {}
     for char in text:
         char = char.lower()
-        if char in char_totals:
-            char_totals[char] +=1
-        else:
-            char_totals[char] = 1
+        if char.isalpha():
+            if char in char_totals:
+                char_totals[char] +=1
+            else:
+                char_totals[char] = 1
     return char_totals
-
-
-### below here is all for the organize_letters function, organize in the future ###
 
 def organize_letters(char_totals):
     char_list = [{'char': char, 'num': count} for char, count in char_totals.items()]
     char_list.sort(reverse=True, key=lambda char_dict:char_dict['num'])
     return char_list
-
-
-def sort_on(char_totals):
-    return char_totals["num"]
-
-def organize_dictionaries(char_totals):
-    char_totals.sort(reverse=True, key=sort_on)
-    return char_totals
-    
 
 if __name__ == "__main__":
     main()
